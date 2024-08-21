dc5260b403c2f64f175a48f87ff5c7792ec52bbe
Heron Boares <heronboares@gmail.com>
2024-08-21 12:57:54 -0300

    Criação do script SQL com o comando para criar o banco de dados e a tabela de carnes

---
+++ banco.sql
@@ -0,0 +1,10 @@
+create table carnes (
+
+	id int primary key auto_increment,
+	valor_total DECIMAL(10, 2) not NULL,
+	qtd_parcelas int not NULL,
+	data_primeiro_vencimento date not NULL,
+	periodicidade varchar(10) not NULL,
+	valor_entrada DECIMAL(10, 2)
+	
+);
