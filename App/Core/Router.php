a2a84e6e1c54290aea7cf0aeb68526a1bf6c402a
Heron Boares <heronboares@gmail.com>
2024-08-21 11:31:34 -0300

    Adição das rotas e direcionamento para o respectivo controller no core Router

--- App/Core/Router.php
+++ App/Core/Router.php
@@ -18,9 +18,63 @@ class Router{
 
         $this->method = $_SERVER["REQUEST_METHOD"];
 
+        if( $this->method == 'GET' && empty($url[1]) ){
 
-        // var_dump($url);
-        // die();
+            http_response_code(200);
+            echo json_encode([
+                "Nome" => "API restfull, para gerenciamento de parcelas de carnês.",
+                "Instrução" => "Acesse a documentação para descobrir os endpoints disponíveis",
+                "Documentação" => "https://github.com/HeronPBV/Carnes-restfull-API"
+            ]);
+            
+            exit;
+
+        }elseif( $this->isRouteNotValid($this->method, $url[1]) ){
+
+            http_response_code(404);
+            echo json_encode(["Erro" => "Rota inválida"]);
+            
+            exit;
+
+        }else{
+
+            $this->controller = 'CarneController';
+
+        }
+
+        require_once "../App/Controllers/" . $this->controller . ".php";
+
+        $this->controller = new $this->controller;
+
+
+        switch($this->method){
+
+            case "GET":
+
+                $this->controllerMethod = "find";
+                $this->params = [(int)$url[1]];
+
+                break;
+
+            case "POST":
+            
+                $this->controllerMethod = "store";
+                break;
+
+            default: 
+                
+                http_response_code(405);
+                echo json_encode(["Erro" => "Método não suportado"]);
+                exit;
+                break;
+
+        }
+
+        call_user_func_array([$this->controller, $this->controllerMethod], $this->params);
+    }
+
+    private function isRouteNotValid($method, $urlArg){
+        return (!empty($urlArg) && (int)$urlArg == 0) || ($method == 'POST' && !empty($urlArg));
     }
 
     private function parseURL(){
