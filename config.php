3a8e5f12e2cd539aea0b096c0e12a476bd95bf60
Heron Boares <heronboares@gmail.com>
2024-08-21 11:12:21 -0300

    Commit inicial, adição dos arquivos principais e sistemas de pastas

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+    const DBDRIVE = 'mysql';
+    const DBHOST = 'localhost';
+    const DBPORT = '3306';
+    const DBNAME = 'apirestfull';
+    const DBUSER = 'root';
+    const DBPASS = '';
