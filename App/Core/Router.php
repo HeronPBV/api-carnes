5c18972bd747bab67f0b97e77b057af6963e430c
Heron Boares <heronboares@gmail.com>
2024-08-21 11:25:14 -0300

    Configuração do core Router, parseando corretamente a URL

--- App/Core/Router.php
+++ App/Core/Router.php
@@ -4,6 +4,27 @@ namespace App\Core;
 
 class Router{
 
-    
+    private $controller;
+
+    private $method;
+
+    private $controllerMethod;
+
+    private $params = [];
+
+    function __construct(){
+        
+        $url = $this->parseURL();
+
+        $this->method = $_SERVER["REQUEST_METHOD"];
+
+
+        // var_dump($url);
+        // die();
+    }
+
+    private function parseURL(){
+        return explode("/", $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
+    }
 
 }
